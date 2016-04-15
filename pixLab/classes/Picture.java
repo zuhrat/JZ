be9d2b09ffffb7c558c95b2f86be470aebbf0cc7
zuhrat <ztukhtamishev@stuy.edu>
2016-04-14 23:49:14 -0400

    Update Picture.java

--- pixLab/classes/Picture.java
+++ pixLab/classes/Picture.java
@@ -134,6 +134,23 @@ public class Picture extends SimplePicture
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
     * from left to right */
+    public void mirrorVerticalRightToLeft()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel leftPixel = null;
+    Pixel rightPixel = null;
+    int width = pixels[0].length;
+    for (int row = 0; row < pixels.length; row++)
+    {
+      for (int col = 0; col < width / 2; col++)
+      {
+        leftPixel = pixels[row][col];
+        rightPixel = pixels[row][width - 1 - col];
+        leftPixel.setColor(rightPixel.getColor());
+      }
+    }
+  }
+  
   public void mirrorVertical()
   {
     Pixel[][] pixels = this.getPixels2D();
