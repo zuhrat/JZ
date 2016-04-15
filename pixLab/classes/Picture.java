4c007c2547e86c0dbe083651036369f2c1cc7820
zuhrat <ztukhtamishev@stuy.edu>
2016-04-14 23:53:12 -0400

    Update Picture.java

--- pixLab/classes/Picture.java
+++ pixLab/classes/Picture.java
@@ -168,6 +168,23 @@ public class Picture extends SimplePicture
     } 
   }
   
+   public void mirrorHorizontal()
+  {
+      Pixel[][] pixels = this.getPixels2D();
+      Pixel topPixel = null;
+      Pixel bottomPixel = null;
+      int height = pixels.length;
+      for (int row = 0; row < height; row++)
+      {
+          for (int col = 0; col < pixels[0].length; col++)
+          {
+              topPixel = pixels[row][col];
+              bottomPixel = pixels[height - 1 - row][col];
+              bottomPixel.setColor(topPixel.getColor());
+          }
+      }
+  }
+  
   /** Mirror just part of a picture of a temple */
   public void mirrorTemple()
   {
